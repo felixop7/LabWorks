f15f582d16e16aa0d3629d8d22f1dbf3e8b41105
Roshan Sahani <roshansahani226@gmail.com>
2023-05-21 05:38:17 +0545

    lab5_added

---
+++ Lab5/insertionsort.cpp
@@ -0,0 +1,28 @@
+#include "Sort.h"
+#include <iostream>
+using namespace std;
+
+void InsertionSort(int* arr, int size){
+    for(int i=0;i<size-1;i++){
+            int a=0;
+            for(int j=0;j<=i;j++){
+                    if (arr[i+1]<arr[j]){
+                        a=1;
+                    }
+                    if(a==1){
+                        arr[j]=arr[j]^arr[i+1];
+                        arr[i+1]=arr[j]^arr[i+1];
+                        arr[j]=arr[j]^arr[i+1];
+                    }
+
+            }
+
+
+
+
+    }
+    return;
+
+
+
+};
